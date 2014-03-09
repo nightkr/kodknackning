535ea8660132ea4a10857ea210ce0607108b480f
Teo Klestrup Röijezon <teozkr@gmail.com>
2014-03-09 18:44:58 +0100

    Added routes to conventions and about pages

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -9,4 +9,12 @@ object Application extends Controller {
     Ok(views.html.index("Your new application is ready."))
   }
 
+	def about = Action {
+		Ok(views.html.content.about())
+	}
+
+	def conventions = Action {
+		Ok(views.html.content.conventions())
+	}
+
 }
