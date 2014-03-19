e47e83221b828d0727a538edab8c63dabcbe8278
Teo Klestrup Röijezon <teo@nullable.se>
2014-03-19 17:11:14 +0100

    Renamed repl to hello world, added link to about page

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -29,9 +29,9 @@ object Application extends Controller {
       Ok(views.html.content.gettingstarted())
   }
 
-  def repl = Action {
+  def helloWorld = Action {
     implicit req =>
-      Ok(views.html.content.repl())
+      Ok(views.html.content.helloWorld())
   }
 
 }
